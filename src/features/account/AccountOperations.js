10ea8c2a834c1aa104ab96deb5ebdcd2d3d4b16b
Ebube Williams <chidiekwebelam@gmail.com>
2026-03-02 14:10:42 +0100

    Making an API Call With Redux Thunks

--- src/features/account/AccountOperations.js
+++ src/features/account/AccountOperations.js
@@ -14,6 +14,7 @@ function AccountOperations() {
     loan: currentLoan,
     loanPurpose: currentLoanPurpose,
     balance,
+    isLoading,
   } = useSelector((store) => store.account);
 
   console.log(balance);
@@ -21,8 +22,9 @@ function AccountOperations() {
   function handleDeposit() {
     if (!depositAmount) return;
 
-    dispatch(deposit(depositAmount));
+    dispatch(deposit(depositAmount, currency));
     setDepositAmount("");
+    setCurrency("");
   }
 
   function handleWithdrawal() {
@@ -64,7 +66,9 @@ function AccountOperations() {
             <option value="GBP">British Pound</option>
           </select>
 
-          <button onClick={handleDeposit}>Deposit {depositAmount}</button>
+          <button onClick={handleDeposit} disabled={isLoading}>
+            {isLoading ? "Converting..." : `Deposit ${depositAmount}`}
+          </button>
         </div>
 
         <div>
@@ -95,14 +99,14 @@ function AccountOperations() {
           <button onClick={handleRequestLoan}>Request loan</button>
         </div>
 
-    {currentLoan > 0 && (
-        <div>
-          <span>
-            Pay back ${currentLoan} ({currentLoanPurpose})
-          </span>
-          <button onClick={handlePayLoan}>Pay loan</button>
-        </div>
-      )}
+        {currentLoan > 0 && (
+          <div>
+            <span>
+              Pay back ${currentLoan} ({currentLoanPurpose})
+            </span>
+            <button onClick={handlePayLoan}>Pay loan</button>
+          </div>
+        )}
       </div>
     </div>
   );
--- src/features/account/accountSlice.js
+++ src/features/account/accountSlice.js
@@ -1,3 +1,4 @@
+
 const initialStateAccount = {
   balance: 0,
   loan: 0,
@@ -7,7 +8,7 @@ const initialStateAccount = {
 export default function accountReducer(state = initialStateAccount, action) {
   switch (action.type) {
     case "account/deposit":
-      return { ...state, balance: state.balance + Number(action.payload) };
+      return { ...state, balance: state.balance + Number(action.payload), isLoading: false };
     case "account/withdraw":
       return { ...state, balance: state.balance - Number(action.payload) };
     case "account/requestLoan":
@@ -25,14 +26,29 @@ export default function accountReducer(state = initialStateAccount, action) {
         loanPurpose: "",
         balance: state.balance - Number(state.loan),
       };
+    case "account/convertingCurrency":
+      return { ...state, isLoading: true };
 
     default:
       return state;
   }
 }
 
-export function deposit(amount) {
-  return { type: "account/deposit", payload: amount };
+export function deposit(amount, currency) {
+  if (currency === "USD") return { type: "account/deposit", payload: amount };
+
+  return async function (dispatch, getState) {
+    dispatch({ type: "account/convertingCurrency" });
+    // API Call
+    const res = await fetch(
+      `https://api.frankfurter.app/latest?amount=${amount}&from=${currency}&to=USD`,
+    );
+    const data = await res.json();
+    const converted = data.rates.USD;
+
+    // return action
+    dispatch({ type: "account/deposit", payload: converted });
+  };
 }
 
 export function withdraw(amount) {
