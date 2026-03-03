2fc1e85ace5903dfff7a605a302f6fd7de49af24
Ebube Williams <chidiekwebelam@gmail.com>
2026-03-03 05:47:35 +0100

    The Redux DevTools

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import App from "./App";
 
 import store from "./store";
 
-store.dispatch({ type: "account/deposit", payload: 500 });
+// store.dispatch({ type: "account/deposit", payload: 500 });
 console.log(store.getState());
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
